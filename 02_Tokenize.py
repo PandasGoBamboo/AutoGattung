3036799007da1a683105af4c41f3d802728c2a8b
PandasGoBamboo <t.schumacher91@gmx.de>
2022-11-28 11:22:57 +0100

    Push

---
+++ 02_Tokenize.py
@@ -0,0 +1,134 @@
+import nltk
+from nltk import pos_tag, sent_tokenize, word_tokenize
+from nltk.stem import SnowballStemmer
+import pandas as pd 
+import numpy as np
+import pickle
+from ClassifierBasedGermanTagger.ClassifierBasedGermanTagger import ClassifierBasedGermanTagger
+from germalemma import GermaLemma
+from datetime import datetime
+
+########################## Funktionen
+
+# POS-Tagging
+def TagPOS_Text(text_list):
+    pos_list = []
+    for row in text_list:
+        new_row = row.split(' ')
+        tagged_sent = tagger.tag(new_row)
+        pos_list.append(tagged_sent)
+    return pos_list
+
+# Lemmatisierung
+def lemmatizeText(text_list):
+    lemmatized = []
+    for lists in text_list:
+        lemmasOF = []
+        for tuples in lists:
+            try:
+                lemma = lemmatizer.find_lemma(tuples[0], tuples[1])
+                lemmasOF.append(lemma)
+            except ValueError:
+                lemmasOF.append(tuples[0])
+                continue
+        lemmatized.append(" ".join(lemmasOF))
+    return lemmatized
+
+# Tokenisierung
+def tokenizeText(text_list):
+
+    stripped = []
+    stemmed = []
+
+    # Initialisiert Snowballstemmer
+    stemmer = SnowballStemmer('german')
+
+    for sendung in text_list:
+
+        sent_of_sendung = []
+        #stem_of_sendung = []
+
+        # Zerlegt jeden Text in Sätze
+        tok_sentences = sent_tokenize(sendung, language='german')
+
+        # Tokenisiert jedes Wort in jedem Satz
+        for satz in tok_sentences:
+
+            # Tokenisierung
+            words = word_tokenize(satz)
+
+            # Strip Satzzeichen
+            new_words= [word for word in words if word.isalnum()]
+            
+            # Joined tokenisierte Wörter jedes Satzes einer Sendung
+            sent_of_sendung.append(" ".join(new_words))
+
+            # Stemming
+            #stemmed_words = [stemmer.stem(word) for word in new_words]
+            #stem_of_sendung.append(" ".join(stemmed_words))
+            
+        # Joined tokensierte Sätze einer Sendung
+        stripped.append(" ".join(sent_of_sendung))
+
+        # Joined gestemmte Sätze einer Sendung
+        #stemmed.append(" ".join(stem_of_sendung))
+   
+    return [
+        stripped #,
+        #stemmed
+    ]
+
+# Counter für Dauer von Skriptausführung
+startTime = datetime.now()
+
+# Konsolenoutput
+print('ich roedel....')
+
+# Öffnet trainierten Korpus zum POS-Tagging    
+with open('nltk_german_classifier_data.pkl', 'rb') as f:
+    tagger = pickle.load(f)
+
+# Einstellung um alle Dokumente zu mergen
+data = pd.read_pickle('Filename.pkl')
+
+# Ausführung des Codes für jeden Namen in der Liste
+text = data['volltext']
+titel = data['haupt_titel']
+sonst_titel = data['sonst_titel']
+
+
+
+# Tokenisierung 
+transformed_text = tokenizeText(text)
+transformed_titel = tokenizeText(titel)
+transformed_sonst_titel = tokenizeText(sonst_titel)
+
+ # Fügt Dataframe Spalten hinzu
+data['stripped_titel'] = transformed_titel[0]
+#data['stemmed_titel'] = transformed_titel[1]
+data['stripped_sonst_titel'] = transformed_sonst_titel[0]
+data['stripped_text'] = transformed_text[0]
+#data['stemmed_text'] = transformed_text[1]
+
+data.to_pickle('more_stripped_raw_data.pkl')
+print('Tokenized')
+
+
+############################ POS-Tagging
+
+pos = TagPOS_Text(data['stripped_text'])
+
+# Fügt Dataframe Spalten hinzu
+#data['tagged'] = pos (ACHTUNG: LANGE SÄTZE SCHRÄNKEN DIE SPEICHERUNG EIN)
+
+############################ Lemmatisierung
+lemmatizer = GermaLemma()
+
+lemmatized = lemmatizeText(pos)
+
+# Fügt Dataframe Spalten hinzu
+data['lemma'] = lemmatized
+
+data.to_pickle('Filename.pkl')
+
+print(datetime.now() - startTime)
---
+++ 03_Classify.py
@@ -0,0 +1,114 @@
+import pandas as pd 
+import pickle
+import nltk
+from nltk.corpus import stopwords
+from datetime import datetime
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics import confusion_matrix, classification_report, plot_confusion_matrix 
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_predict, GridSearchCV, KFold 
+
+##### Zähler für Dauer von Skriptausführung
+startTime = datetime.now()
+print(startTime)
+
+############################ Datei laden
+file = 'C:/Users/Path/to/File'
+
+### Stoppwortliste laden
+nltk.download('stopwords')
+german_stop_words = stopwords.words('german') 
+
+print('Ich roedel......')
+
+### 
+#liste = ['CHR', 'ESS', 'REP', 'REZ', 'KOM', 'INT', 'GRF'] 
+liste = ['WRT', 'SAT', 'LEX', 'LES', 'FRG', 'CHR', 'VON', 'SDK', 'PER', 'OFB', 'ANZ'] 
+data = pd.read_pickle(file)
+
+new = data[~data['pform'].isin(liste)]
+#new = data[data['pform'].isin(liste)]
+
+#new.reset_index(drop=True, inplace=True)
+
+
+#all = new
+all = new.sample(n=50000, random_state=1)
+#all = data.sample(n=50000, random_state=1)
+
+#all['stripped_text'] = all['stripped_text'].str.split().str[:100].str.join(' ')
+#all['stripped_text'] = all['stripped_text'].apply(lambda x: ' '.join(x.split(' ')[:300]))
+
+texts = all['stripped_text']
+#texts = all['stripped_titel'] + all['stripped_sonst_titel'] + all['stripped_text']
+
+# Trainingsklassenaus
+y = all['pform']
+
+# Trainingsspalten
+X = texts
+
+print('X und Y geladen')
+
+
+############################ Cross-Validation mit Vektorisierung, Klassifikation und Parametertuning
+
+##### Initialisierung des KFold
+# n_split =   Anzahl, wie oft Daten aufgeteilt werden
+# shuffle =   Randomisiert die Daten. Da sie in der Datei nach Klassen sortiert sind,
+#             muss dieser Param gesetzt werden.
+# rnd_state = Eine beliebige Zahl. Dadurch wird gesichert, dass immer dieselbe Aufteilung 
+#             erfolgt. 
+
+kf = KFold(n_splits=10, shuffle=True, random_state=42)
+
+############################ Pipeline Bestandteile
+
+#tfidf = TfidfTransformer()
+vectorizier = TfidfVectorizer()
+
+#### Klassifikator
+classifier = SGDClassifier()
+
+scaler = StandardScaler()
+
+pipe = Pipeline([
+    ('vect', vectorizier),
+    ('scaler', scaler),
+    ('class', classifier)
+    ])
+
+params = {
+    "vect__lowercase": [False],
+    "vect__stop_words": [german_stop_words],
+    "scaler__with_mean": [False],
+    'class__loss': ['hinge'], #Support Vektor Maschine
+    'class__max_iter': [5000]
+    }
+
+print('Ich fitte jetzt')
+clf = GridSearchCV(pipe, param_grid = params, cv = kf )
+clf.fit(X, y)
+print(clf.best_params_)
+
+print('Ich predicte jetzt')
+y_pred = cross_val_predict(clf, X, y)
+print(' ')
+
+############################ Output und Visualisierungen
+print('Report für: ' + 'stripped')
+print(' ')
+plot_confusion_matrix(clf, X, y_pred)  
+plt.show()
+print(' ')
+print(classification_report(y, y_pred))
+
+
+filename = 'Filename.sav'
+pickle.dump(clf, open(filename, 'wb'))
+
+print(datetime.now())
